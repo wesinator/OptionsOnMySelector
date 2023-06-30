1581fcccde24b670f8e708902d6dbb7cb89a3729
wesinator <5124946+wesinator@users.noreply.github.com>
2023-06-30 16:42:57 -0400

    chrome mv3

---
+++ chrome_mv3/getHtmlOptions.js
@@ -0,0 +1,46 @@
+var selects = document.getElementsByTagName("select");
+//console.log("selects:", selects);
+if (selects.length) {
+  for (var select of selects) {
+    //if (select.id) {
+      var getSelects = confirm(`Would you like to download the option list "${select.name || select.id}" to a text file ?`);
+      if (getSelects) {
+        items = getSelectOptionItems(select);
+        console.log(items.length, " items")
+
+      	if (items.length) {
+        	var filename = `options_${select.id || select.name}.txt`;
+        	arrayToFile(items, filename);
+      	}
+      }
+    //}
+  }
+}
+
+function getSelectOptionItems(select) {
+  var items = [];
+
+  for (var item of select) {
+    if (item) {
+      console.log(item.text);
+      items.push(item.text);
+    }
+  }
+
+  return items;
+}
+
+function arrayToFile(list, filename) {
+  var contents = "/* retrieved from: " + document.URL + " */\n\n" + list.join('\n');
+  fileDataDownload(contents, filename, "text/plain");
+}
+
+function fileDataDownload(contents, name, mimeType) {
+  // https://stackoverflow.com/questions/34101871/save-data-using-greasemonkey-tampermonkey-for-later-retrieval
+  var a = document.createElement("a");
+
+  a.href = `data:${mimeType};charset=utf-8,` + encodeURIComponent(contents);
+
+  a.download = name;
+  a.click();
+}
