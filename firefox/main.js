e4002733d498ebf545f05dd6e521c67046561253
wesinator <5124946+wesinator@users.noreply.github.com>
2023-07-05 08:52:43 -0400

    minor
    0.3.2

--- firefox/main.js
+++ firefox/main.js
@@ -1,7 +1,7 @@
 // https://stackoverflow.com/a/71584393 getting activeTab for addListener
-chrome.action.onClicked.addListener(async (tab) => {
+chrome.action.onClicked.addListener((tab) => {
   //console.log("executing the browser action");
-  browser.scripting.executeScript({
+  chrome.scripting.executeScript({
     target: {tabId: tab.id, allFrames: true},
     files: ["getHtmlOptions.js"],
   });
